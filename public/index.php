c89be03aa9ff4855ff683d7e646a6546e75d9acf
wilsantos99_1 <wilsantos99@hotmail.com>
2015-08-05 13:05:12 +0000

    Fix Bug: Separando arquivos públicos dos arquivos privados.

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+define('PATH', dirname(__DIR__));
+
+if(file_exists(PATH.'/vendor/autoload.php')===false){
+    exit("Please, install the composer.");
+}
+
+require PATH.'/vendor/autoload.php';
+require PATH.'/config.php';
+
+use MVC\Core\Controller;
+
+// Inicializa a aplicação
+$app = new Controller();
+
+$app->init();
