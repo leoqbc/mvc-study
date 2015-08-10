678b42755631c3d341bd5cc49f1bf7cf080312fe
Wilson Divino <wilson_divino@hotmail.com>
2015-08-10 20:29:34 -0300

    Validação do Composer

--- public/index.php
+++ public/index.php
@@ -1,6 +1,13 @@
 <?php
-require 'vendor/autoload.php';
-require 'config.php';
+
+define("PATH", dirname(__DIR__));
+
+if(file_exists(PATH."/vendor/autoload.php")===false){
+  exit("Please, install composer.");
+}
+
+require PATH.'/vendor/autoload.php';
+require PATH.'/config.php';
 
 use MVC\Core\Controller;
 
